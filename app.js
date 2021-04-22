c8ca8979c325c5a9a870fc19f8b0b0cd29045789
Ebrahim Ali <ebrahimali.cse.ru@gmail.com>
2021-04-23 04:08:32 +0600

    implement rate limiting with express-rate-limit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const cors = require('cors')
 const express = require('express');
 const cookieParser = require('cookie-parser')
+const rateLimit = require('express-rate-limit')
 const app = express();
 
 const bookRoute = require('./route/bookRouter')
@@ -11,6 +12,15 @@ const AppError = require('./utils/appError');
 const filterRoute = require('./route/filterRouter');
 
 //GLOBAL MIDDLEWARE
+//API REQUEST LIMITING TO PREVENT HEAVEY REQUEST 
+const limiter = rateLimit({
+    max: 1000,
+    windowMs: 60 * 60 * 1000,
+    message: 'To many request! please try later 1 hour later :('
+})
+
+app.use('/api', limiter)
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:3000' }));
